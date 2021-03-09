d4181a6162248d106e0ad9d98b6ca4c9dc3cc322
ganwang <74584251+ganwang-bit@users.noreply.github.com>
2021-03-09 20:34:07 +0800

    21-3-9

---
+++ 21-3-9/main.c
@@ -0,0 +1,155 @@
+//#include <stdio.h>
+//#include <stdlib.h>
+//
+//int main()
+//{
+//    printf("Hello world!\n");
+//    return 0;
+//}
+//#include<stdio.h>
+//#include<windows.h>
+//int main()
+//{
+//    int a=3;
+//    printf("%d",sizeof(++a));
+//    printf("%d",a);
+//    return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//    int a=1,d;
+//    float b=20,c;
+//    d=a+b;
+//    c=a+b;
+//    printf("%d%f",d,c);
+//    return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//    double x = 2.5, y = 4.7;
+//int a = 7;
+//
+//printf("%.1f", x + a%3 * (int)(x+y) % 2 / 4);
+//return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//    double a=10.2;
+//    a++;
+//    printf("%lf",a);
+//    return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//    int a,b;
+//    printf("%d",(a=2,b=5,a>b?a++:b++,a+b));
+//    return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//    printf("%d", 2||3+10&&2);
+//    return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//    int a=10, b=20,c;
+//    c=(a%b<1)||(a/b>1);
+//    printf("%d %d %d\n", a, b, c);
+//    return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//    int m=5;
+//    int y=2;
+//    y+=m>y+2||--m;
+//    printf("%d#%d#", m, y );
+//    return 0;
+//
+//}
+//#include<stdio.h>
+//int main()
+//{
+//
+//    int money, year;
+//double rate;
+//scanf("%d %d %lf", &money, &year, &rate);
+//printf("%d#%d#%.3f", money, year, rate);
+//return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//    int x=-2;
+//    printf("%8lx",x);
+//    return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//    short int a;
+//int b = 65536;
+//
+//a = b;
+//printf("%d\n", a);
+//    return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//    printf("%uc",'a');
+//    return 0;
+//}
+//#include <stdio.h>
+//
+//int main()
+//{
+//    int x;
+//    scanf("%d", &x);
+//    printf("%08X\n", x);
+//printf("%d",printf("-123"));
+//printf("%c", '\Xf');
+//    return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//    int n;
+//    scanf("%d",&n);
+//    putchar(n-1);
+//    putchar(n+1);
+//    return 0;
+//}
+//#include<stdio.h>
+//#include<math.h>
+//int main()
+//{
+//    double n;
+//    scanf("%lf",&n);
+//    printf("%.6lf",log(n)+sin(pow(n,3.5))+exp(2*n)-1);
+//    return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//    float a,b,c,d,x;
+//    scanf("%f",&a);
+//    scanf("%f %f %f",&b,&c,&d);
+//    x=d*a/(b+c);
+//    printf("%.2lf",(int)(x*100+0.5)/100.0);
+//    return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//    int a,b;
+//    scanf("%o%X",&a,&b);
+//    printf("%d",a+b);
+//    return 0;
+//}
